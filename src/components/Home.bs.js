19f0af2b42848b1fccc074787f3aa23ea0265922
Kurt M. Ma. Coll <kurt_ma.coll@live.com>
2021-08-17 17:40:37 +0800

    Add the initial HTML content

--- src/components/Home.bs.js
+++ src/components/Home.bs.js
@@ -4,9 +4,26 @@ import * as React from "react";
 
 function Home(Props) {
   return React.createElement("div", {
-              className: "text-green-600 hover:text-red-700",
               id: "home"
-            }, "Perhaps");
+            }, React.createElement("div", {
+                  id: "top-bar"
+                }, React.createElement("button", undefined), React.createElement("div", {
+                      id: "top-email"
+                    }), React.createElement("div", {
+                      id: "socmed-links"
+                    }, React.createElement("img", {
+                          src: "/public/icons/iconmonstr-github-1.svg"
+                        }), React.createElement("img", {
+                          src: "/public/icons/iconmonstr-linkedin-3.svg"
+                        }))), React.createElement("div", {
+                  id: "home-title"
+                }, React.createElement("div", {
+                      id: "home-name"
+                    }, "Kurt M. Ma. Coll"), React.createElement("div", {
+                      id: "description"
+                    }, "A software developer who specialises in web backend and databases")), React.createElement("div", {
+                  id: "sdtc"
+                }, "(Scroll down to continue.)"));
 }
 
 var make = Home;
--- src/components/Home.res
+++ src/components/Home.res
@@ -1,6 +1,24 @@
 @react.component
 let make = () => {
-  <div id="home" className={"text-green-600 hover:text-red-700"}>
-    {React.string("Perhaps")}
+  let myName = "Kurt M. Ma. Coll"
+  let myDesc = "A software developer who specialises in web backend and databases"
+  <div id="home">
+    <div id="top-bar">
+      <button></button>
+      <div id="top-email"></div>
+      <div id="socmed-links">
+        <img src="/public/icons/iconmonstr-github-1.svg"/>
+        <img src="/public/icons/iconmonstr-linkedin-3.svg"/>
+      </div>
+    </div>
+    <div id="home-title">
+      <div id="home-name">
+        {React.string(myName)}
+      </div>
+      <div id="description">
+        {React.string(myDesc)}
+      </div>
+    </div>
+    <div id="sdtc">{React.string("(Scroll down to continue.)")}</div>
   </div>
 }
